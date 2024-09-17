d9eaac930cb3ca48f1c8af5a614a38b0205d249a
HarishR2002 <harish30902@gmail.com>
2024-09-17 16:53:30 +0530

    he

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,33 +2,37 @@ import React from 'react';
 import { FaShoppingCart } from 'react-icons/fa'; // Import cart icon
 
 function Navbar({ cartItemsCount }) {
+  const handleLinkClick = (e) => {
+    e.preventDefault(); // Prevent default link behavior
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <a className="navbar-brand" href="#">Shop Homepage</a>
+        <a className="navbar-brand" href="#" onClick={handleLinkClick}>Shop Homepage</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <a className="nav-link" href="#">Home</a>
+              <a className="nav-link" href="#" onClick={handleLinkClick}>Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
+              <a className="nav-link" href="#" onClick={handleLinkClick}>About</a>
             </li>
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="shopDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <a className="nav-link dropdown-toggle" href="#" id="shopDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onClick={handleLinkClick}>
                 Shop
               </a>
               <div className="dropdown-menu" aria-labelledby="shopDropdown">
-                <a className="dropdown-item" href="#">All Products</a>
-                <a className="dropdown-item" href="#">Popular Items</a>
-                <a className="dropdown-item" href="#">New Arrivals</a>
+                <a className="dropdown-item" href="#" onClick={handleLinkClick}>All Products</a>
+                <a className="dropdown-item" href="#" onClick={handleLinkClick}>Popular Items</a>
+                <a className="dropdown-item" href="#" onClick={handleLinkClick}>New Arrivals</a>
               </div>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="#" onClick={handleLinkClick}>
                 <FaShoppingCart /> {/* Cart icon */}
                 Cart 
                 {cartItemsCount > 0 && (
